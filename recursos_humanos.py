32c4a17c483c70362653125b07dc5f8e8e1bae5b
NicoMalta <niicou96@hotmail.com>
2019-10-03 17:04:05 -0300

    First Commit

---
+++ recursos_humanos.py
@@ -0,0 +1,44 @@
+import psycopg2
+from datetime import datetime
+from random import randrange
+from datetime import timedelta
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime 
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+d1 = datetime.strptime('1/1/2008 1:30 PM', '%m/%d/%Y %I:%M %p')
+d2 = datetime.strptime('1/1/2009 4:50 AM', '%m/%d/%Y %I:%M %p')
+
+try:
+   connection = psycopg2.connect(user="malta",
+                                  password="malta",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="recursos_humanos")
+   cursor = connection.cursor()
+
+   postgres_insert_query = """ INSERT INTO actividad (descripcion, fecha_inicio) VALUES (%s,%s)"""
+   record_to_insert = ('Zumba', random_date(d1, d2))
+   cursor.execute(postgres_insert_query, record_to_insert)
+
+   connection.commit()
+   count = cursor.rowcount
+   print (count, "Record inserted successfully into mobile table")
+
+except (Exception, psycopg2.Error) as error :
+    if(connection):
+        print("Failed to insert record into mobile table", error)
+
+finally:
+    #closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
